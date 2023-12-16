2dce4af752841495fafb2a6c8187326185ba319a
Valentino Villalba <valentinovillalba61@gmail.com>
2023-12-16 17:37:53 -0300

    enviamos un mensaje mediante props al componente informcaion

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import Informacion from "./components/Informacion";
 import 'bootstrap/dist/css/bootstrap.min.css' 
 
 function App() {
+  //aqui vamos a agreagar la logica del componente
   return (
+    //aqui va un poquito de logica
+    //lueho va el maquetadp del componente
     <section className="container my-4">
       <h1 className="text-center display-3">PrimerApp React</h1>
-      <Informacion></Informacion>
+      <Informacion tituloSecundario='Este titulo fue creado con un props'></Informacion>
       {/*<Informacion/>*/}
     </section>
   );
