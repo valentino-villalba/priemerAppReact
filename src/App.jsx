be6637eb241527304c450e4a8357dfe309940880
Valentino Villalba <valentinovillalba61@gmail.com>
2023-12-16 17:15:23 -0300

    agregamos una lista con conveptos de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Informacion from "./components/Informacion";
+import 'bootstrap/dist/css/bootstrap.min.css' 
 
 function App() {
   return (
-    <section>
-      <h1>prueba</h1>
+    <section className="container my-4">
+      <h1 className="text-center display-3">PrimerApp React</h1>
       <Informacion></Informacion>
-      <Informacion/>
+      {/*<Informacion/>*/}
     </section>
   );
 }
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,7 +1,16 @@
 const Informacion = () => {
   return (
     <section>
-      <h2>Titulo secundario</h2>
+      <h2>Conceptos basicos de bootstrap</h2>
+      <p>React es una biblioteca de JavaScript desarrollada por Facebook que se utiliza para construir interfaces de usuario interactivas y eficientes. Su enfoque se centra en la creación de componentes reutilizables que representan diferentes partes de la interfaz de usuario. React utiliza un paradigma de programación declarativa, lo que significa que los desarrolladores describen cómo debería ser la interfaz de usuario y React se encarga de manejar los cambios de manera eficiente.</p>
+
+      <ol className="list-group">
+        <li className="list-group-item">Componentes: Son bloques de construcción fundamentales en React. Representan partes específicas de la interfaz de usuario y pueden contener lógica y estado propios.</li>
+        <li className="list-group-item">Propiedades (Props): Son datos que se pasan de un componente padre a un componente hijo. Permiten la comunicación entre componentes.</li>
+        <li className="list-group-item">Estado (State): Es un objeto que representa la información interna de un componente. Cuando el estado de un componente cambia, React se encarga de actualizar la interfaz de usuario de manera eficiente.</li>
+        <li className="list-group-item">Ciclo de vida (Lifecycle): React proporciona métodos que se ejecutan en diferentes etapas del ciclo de vida de un componente, como montaje, actualización y desmontaje. Esto permite realizar acciones específicas en momentos específicos.</li>
+        <li className="list-group-item">JSX (JavaScript XML): Es una extensión de JavaScript que permite escribir código similar a HTML dentro de archivos JavaScript. JSX facilita la creación de elementos de interfaz de usuario en React.</li>
+      </ol>
     </section>
   );
 };
