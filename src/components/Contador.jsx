5b23dbc6cefd5a560ec1e814c764f628990e6c94
Valentino Villalba <valentinovillalba61@gmail.com>
2023-12-17 09:42:23 -0300

    agregamos un satate en el componente contenedor

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
+
 const Contador = () => {
-  let numero = 10;
+//  let numero = 10;
+const [numero, setNumero]= useState(10)
+//numero = numero +1
   return (
     <article className="text-center mt-4">
       <h3>Contador</h3>
       <p className="fw-bold">{numero}</p>
       <button
         className="btn btn-primary"
-        onClick={() => {
-          numero++;
-          console.log(numero);
-        }}
+        onClick={() =>  setNumero(numero + 1)}
       >
         +1
       </button>
